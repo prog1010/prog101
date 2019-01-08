98270840cfb2a4f6834524096256c61f004e6009
Iulia Andra <andra.iulia@gmail.com>
2019-01-08 13:42:26 +0200

    all keys in object

---
+++ homework5/Iulia Andra/all_object_keys.js
@@ -0,0 +1,22 @@
+
+function all_object_keys(obiect){
+ // console.log(Object.keys(obiect));
+// sau 
+
+  for (var k in obiect) {
+  	console.log(k);
+  	if (typeof obiect[k] === "object" )
+  		all_object_keys(obiect[k])
+  }
+
+}
+
+all_object_keys(3);
+
+all_object_keys ({
+	type: {
+		prop1: 1,
+		prop2: 2
+	}, 
+	model: 3, 
+	color: 'blue'});
