82ba2551482037debb4df838c635f0794c4ad82b
Iulia Andra <andra.iulia@gmail.com>
2019-01-08 11:22:09 +0200

    added xmas tree homework

---
+++ homework5/Iulia Andra/xmas_tree.js
@@ -0,0 +1,15 @@
+var height =7;
+
+for (var i=1; i<=height; i++){
+	var rand = "";
+	for (var j=1; j<height-i+1; j++){
+		 rand = rand + " "
+		
+
+	}
+	for (var j=1; j<=(2*i-1); j++){
+		 rand = rand + "*"
+
+	}
+	console.log(rand);
+}
